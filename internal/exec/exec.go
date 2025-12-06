01f2dd292f458eb6358907360f98ea255c37c3ee
Filiberto Fuentes <Filiberto.Fuentes@microsoft.com>
2025-12-05 17:09:29 -0800

    update logic

--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -155,17 +155,11 @@ func SetEnvironmentVariables(cfg *handlersettings.HandlerSettings) (string, erro
 		parameters = append(parameters, cfg.ProtectedSettings.ProtectedParameters...)
 	}
 
-	isFleetDiagnosticsCommandId := false
-	if strings.Contains(cfg.PublicSettings.Source.Script, constants.CommandID_FleetDiagnosticsLinux) {
-		isFleetDiagnosticsCommandId = true
-	}
-
 	for i := 0; i < len(parameters); i++ {
 		name := parameters[i].Name
 		value := parameters[i].Value
 		if value != "" {
-			if name != "" && !isFleetDiagnosticsCommandId {
-				// Named parameters are set as environmental setting except for FleetDiagnosticsLinux
+			if name != "" { // Named parameters are set as environmental setting
 				err = os.Setenv(name, value)
 			} else { // Unnamed parameters go to command args
 				commandArgs += " " + value
